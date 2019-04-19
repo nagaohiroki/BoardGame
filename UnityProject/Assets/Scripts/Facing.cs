c2ec84e4f9d917b8a7cbc9d2ae25daf1a2c24803
nagaohiroki <testplayers0226@gmail.com>
2019-04-19 13:56:33 +0900

    delete old file

--- UnityProject/Assets/Scripts/Facing.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-public class Facing : MonoBehaviour
-{
-	void Update()
-	{
-		var cam = Camera.main;
-		transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
-	}
-}
---
+++ UnityProject/Assets/Scripts/Launcher.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+public class Launcher : MonoBehaviourPunCallbacks
+{
+	[SerializeField]
+	byte mMaxPlayerPerRoom = 0;
+	[SerializeField]
+	Text mUserName = null;
+	[SerializeField]
+	GameObject mLoginForm = null;
+	// ------------------------------------------------------------------------
+	/// @brief ログインボタンをおしたときの動作
+	// ------------------------------------------------------------------------
+	public void Connect()
+	{
+		if(mUserName == null || string.IsNullOrEmpty(mUserName.text))
+		{
+			return;
+		}
+		PhotonNetwork.LocalPlayer.NickName = mUserName.text;
+		if(PhotonNetwork.IsConnected)
+		{
+			PhotonNetwork.JoinRandomRoom();
+		}
+		else
+		{
+			PhotonNetwork.GameVersion = "1";
+			PhotonNetwork.ConnectUsingSettings();
+		}
+		if(mLoginForm != null)
+		{
+			mLoginForm.SetActive(false);
+		}
+	}
+	void Awake()
+	{
+		PhotonNetwork.AutomaticallySyncScene = true;
+	}
+	public override void OnConnectedToMaster()
+	{
+		PhotonNetwork.JoinRandomRoom();
+		Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+	}
+	public override void OnDisconnected(DisconnectCause cause)
+	{
+	}
+	// 🍜😺
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		var room = new RoomOptions();
+		room.MaxPlayers = mMaxPlayerPerRoom;
+		PhotonNetwork.CreateRoom(null, room);
+	}
+}
