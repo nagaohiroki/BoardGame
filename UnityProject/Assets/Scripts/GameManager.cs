50f6b80fc8010707676fc4ab0b66c3c645017912
nagaohiroki <testplayers0226@gmail.com>
2019-04-26 14:00:06 +0900

    Player

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine;
 public class GameManager : MonoBehaviourPunCallbacks
 {
+	[SerializeField]
+	GameObject mLobbyPlayerPrefab = null;
+	[SerializeField]
+	Transform mCameraHandle = null;
 	public override void OnLeftRoom()
 	{
 		SceneManager.LoadScene(0);
@@ -24,6 +29,24 @@ public class GameManager : MonoBehaviourPunCallbacks
 	}
 	void LoadArena()
 	{
-		PhotonNetwork.LoadLevel("Room" + PhotonNetwork.CurrentRoom.PlayerCount);
+		PhotonNetwork.LoadLevel("Room1");
+	}
+	void Start()
+	{
+		if(!PhotonNetwork.IsConnected)
+		{
+			return;
+		}
+		var player = PhotonNetwork.Instantiate(mLobbyPlayerPrefab.name, new Vector3(0f,5f,0f), Quaternion.identity, 0);
+		LobbyPlayer lobbyPlayre = player.GetComponent<LobbyPlayer>();
+		lobbyPlayre.Initialize(mCameraHandle);
+	}
+	void Update()
+	{
+		if(!PhotonNetwork.IsConnected)
+		{
+			OnLeftRoom();
+		}
+
 	}
 }
--- UnityProject/Assets/Scripts/LobbyPlayer.cs
+++ UnityProject/Assets/Scripts/LobbyPlayer.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
-public class LobbyPlayer : MonoBehaviour
+using Photon.Pun;
+public class LobbyPlayer : MonoBehaviourPun
 {
 	Transform mCameraHandle = null;
+	[SerializeField]
+	Rigidbody mRigidody = null;
 	public void Initialize(Transform inCameraHandle)
 	{
 		mCameraHandle = inCameraHandle;
 	}
 	void Update()
 	{
+		if(!photonView.IsMine && PhotonNetwork.IsConnected)
+		{
+			return;
+		}
 		if(mCameraHandle != null)
 		{
 			mCameraHandle.position = transform.position;
 		}
+		Move();
+	}
+	void Move()
+	{
+		if (mRigidody == null)
+		{
+			return;
+		}
+		var vec = Vector3.zero;
+		vec.x = Input.GetAxis("Horizontal");
+		vec.z = Input.GetAxis("Vertical");
+		mRigidody.AddForce(vec, ForceMode.VelocityChange);
 	}
 }
